7d2b4c1535ac9880327225e5c8b6d63a45a375bc
Omar-Mega-Byte <113197407+Omar-Mega-Byte@users.noreply.github.com>
2025-04-27 18:32:53 +0300

    على بركة الله

---
+++ user-service/src/main/java/com/example/SE_Project/SeProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.SE_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class SeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeProjectApplication.class, args);
+	}
+
+}
